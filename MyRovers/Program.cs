500c26f9fb8a1ec64c6880fd338be3d3d1b9baf0
ebrardd <ebraremine.demircioglu@st.uskudar.edu.tr>
2023-07-28 15:56:45 +0300

    Add project files.

---
+++ MyRovers/Program.cs
@@ -0,0 +1,189 @@
+﻿using MyRover;
+
+namespace MyRovers
+{
+    enum Movement // numaralandırma,sabit liste olusturma belli türdeki degerleri tutmaya yarar.
+    {
+        W,//Yukarı
+        D,//Sag
+        S,//Asagı
+        A,//Sol
+        L,
+        R,
+        M
+    }
+
+    class program
+    {
+
+        static void Main()
+        {
+            string[] MenuChoices = new string[]
+            {
+            "Choice1","Choice2","Choice3" };
+            int e = 0;
+            //choice1=Enter a name,choice2=Move the rovers,choice3=check the rovers
+
+            while (true)
+                while (true)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Welcome to Menu");
+                    Console.WriteLine();
+
+                    for (int i = 0; i < MenuChoices.Length; i++)
+                    {
+                        if (e == i)
+                        {
+                            Console.WriteLine(" " + MenuChoices[i] + "  {");
+                        }
+                        else
+                        {
+                            Console.WriteLine(MenuChoices[i]);
+                        }
+                    }
+                    var key = Console.ReadKey();
+
+                    if (key.Key == ConsoleKey.DownArrow && e != MenuChoices.Length - 1)
+                    {
+                        e++;
+                    }
+                    else if (key.Key == ConsoleKey.UpArrow && e
+                               >= 1)
+                    {
+                        e--;
+                    }
+                    else if (key.Key == ConsoleKey.Enter)
+                    {
+                        switch (e)
+                        {
+                            case 0:
+                                MenuChoice1();
+                                break;
+                            case 1:
+                                MenuChoice2();
+                                break;
+                            case 2:
+                                MenuChoice3();
+                                break;
+                            case 3:
+                                MenuChoice4();
+                                break;
+                        }
+                    }
+                }
+        }
+
+        public static void MenuChoice1()
+        {
+
+            //class2.second();
+            Console.Clear();
+
+            Console.ReadKey();
+        }
+
+        public static void MenuChoice2()
+        {
+
+
+
+            Console.Clear();
+
+            Console.ReadKey();
+        }
+        public static void MenuChoice3()
+        {
+            Console.Clear();
+
+            Console.ReadKey();
+
+
+        }
+
+        public static void MenuChoice4()
+        {
+
+            Console.Clear();
+
+            Console.WriteLine("Press Enter To Exit The Menu");
+            Console.ReadKey();
+            Console.Clear();
+            Environment.Exit(1);
+
+            Console.Clear();
+            Console.SetWindowSize(Plateau.Width + Plateau.InfoWidth, Plateau.Height);
+            Console.SetBufferSize(Plateau.Width + Plateau.InfoWidth, Plateau.Height);
+            Rover rover = new Rover();
+            Movement movement = new Movement();
+
+            ulong x = 0;
+            ulong y = 0;
+            x++;
+            y++;
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo keyInfo;
+                while ((keyInfo = Console.ReadKey(true)).Key != ConsoleKey.Escape)
+                {
+                    switch (keyInfo.Key)
+                    {
+                        case ConsoleKey.L:
+                            Console.ReadKey();
+                            Console.WriteLine("turn left");
+                            break;
+                        case ConsoleKey.R:
+                            Console.ReadKey();
+                            Console.WriteLine("turn right");
+                            break;
+                        case ConsoleKey.M:
+                            Console.ReadKey();
+                            Console.WriteLine("Move 1 unit");
+                            break;
+                        case ConsoleKey.W:
+                            Console.ReadKey();
+                            Console.WriteLine("Move Up ");
+                            break;
+                        case ConsoleKey.D:
+                            Console.ReadKey();
+                            Console.WriteLine("Move right");
+                            break;
+
+                        case ConsoleKey.S:
+                            Console.ReadKey();
+                            Console.WriteLine("Move down");
+                            break;
+
+                        case ConsoleKey.A:
+                            Console.ReadKey();
+                            Console.WriteLine("Move left +1");
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid key.");
+                            break;
+
+                    }
+                }
+                while ((keyInfo = Console.ReadKey(true)).Key != ConsoleKey.Escape)
+                {
+                    Console.WriteLine("Press a key");
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
+ 
---
+++ MyRovers/Rovers.cs
@@ -0,0 +1,60 @@
+﻿using MyRovers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRover
+{
+    public class Rover
+    {
+        private const ulong _height = 9000000000000000000, _length = 9000000000000000000;
+        private int _cord_X;
+        private int _cord_Y;
+
+        public int Cord_X
+        {
+            get { return _cord_X; } // x koordinatı okunabilir (get)
+            set { _cord_X = value; } // x koordinatı değiştirilebilir (set)
+        }
+        public int Cord_Y
+        {
+            get { return _cord_Y; }
+            set { _cord_Y = value; }
+        }
+        public ulong Height
+        {
+            get { return _height; }
+        }
+
+        public ulong Length
+        {
+            get { return _length; }
+        }
+       internal  class Coordinate
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public Coordinate(int x, int y)
+            {
+                X = x;
+               Y = y;
+            }
+        }
+
+        static bool CanMove(Coordinate c)
+        {
+            if (c.X < 0 || c.X >= Console.WindowWidth)
+                return false;
+
+            if (c.Y < 0 || c.Y >= Console.WindowHeight)
+                return false;
+
+            return true;
+        }
+    }
+}
